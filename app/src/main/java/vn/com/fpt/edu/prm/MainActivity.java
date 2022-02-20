8bc08dc444375bcb3c315a2d4e2f2bf5a30fb160
Lâm <hoanglammaster@gmail.com>
2022-02-20 18:52:45 +0700

    Init Project

---
+++ app/src/main/java/vn/com/fpt/edu/prm/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.com.fpt.edu.prm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
